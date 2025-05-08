fb81954a007ff0f9fe4fa5c3500bd7bac885d49e
Matilda Erenius <matildaerenius@hotmail.com>
2025-05-08 11:16:56 +0200

    chore: configure MySQL datasource and insert seed data

---
+++ backend/sthlm-exit/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO station (name, line, latitude, longitude)
+VALUES ('T-Centralen', 'blå', 59.3302, 18.0595);
+
+INSERT INTO exit (name, latitude, longitude, train_position, station_id)
+VALUES
+    ('Sergels torg', 59.3326, 18.0649, 'fram', 1),
+    ('Vasagatan', 59.3308, 18.0566, 'bak', 1),
+    ('Pendeltåg', 59.3293, 18.0572, 'mitten', 1);
