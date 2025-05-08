e492dce3d0466a04672c69fbacdd06274113b2be
Matilda Erenius <matildaerenius@hotmail.com>
2025-05-08 10:49:49 +0200

    feat: init SpringBoot backend

---
+++ backend/sthlm-exit/src/main/java/com/matildaerenius/sthlmexit/SthlmExitApplication.java
@@ -0,0 +1,13 @@
+package com.matildaerenius.sthlmexit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SthlmExitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SthlmExitApplication.class, args);
+	}
+
+}
---
+++ backend/sthlm-exit/src/test/java/com/matildaerenius/sthlmexit/SthlmExitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matildaerenius.sthlmexit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SthlmExitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
